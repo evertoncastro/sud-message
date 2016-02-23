32c3d8992769a0071969dc2bf25ebb0bca38817d
Dyego Rodrigo <dyego9444@hotmail.com>
2016-02-22 22:36:10 -0300

    basic register event method

--- src/models/events.py
+++ src/models/events.py
@@ -4,14 +4,34 @@ import json
 import logging
 import webapp2
 
-
 class Event(ndb.Model):
-    def fakemethod(self):
-        return True
+    dateCreation = ndb.DateTimeProperty(auto_now=False)
+    title = ndb.StringProperty()
+    description = ndb.TextProperty()
+    host = ndb.StringProperty()
+    image = ndb.StringProperty()
 
 class RegisterEvent():
-    def fakemethod(self):
-        return True
+    def handle_auth(self, received_json_data, response_data, user):
+        try:
+            if not received_json_data("title") or not received_json_data("description") or not  received_json_data("host"):
+                response_data["status"] = "MESSAGE INCOMPLETE"
+                response_data["desc"] = "Um ou mais parametros nao foram enviados corretamente, tente novamente mais tarde".decode("latin-1")
+            else:
+               event = Event(
+                 title=received_json_data.get("title"),
+                 description=received_json_data.get("description"),
+                 host=received_json_data.get("host")
+               )
+               if received_json_data.get("image"):
+                   event.image = received_json_data.get("image")
+
+               event.put()
+               response_data["message"] = "Evento criado com sucesso".decode("latin-1")
+               response_data["intern"] = True
+        except:
+            response_data["message"] = "Algo deu errado no cadastro do evento".decode("latin-1")
+            response_data["intern"] = False
 
 class UpdateEvent():
     def fakemethod(self):
@@ -20,4 +40,3 @@ class UpdateEvent():
 class DropEvent():
     def fakemethod(self):
         return True
-
